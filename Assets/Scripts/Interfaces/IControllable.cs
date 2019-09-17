4fc29cba4d5355cc7ae456b59d926577de0ddb59
Simon Meyer <meyersimon8@gmail.com>
2019-09-17 12:03:16 +0200

    Unit Class
    Added Unit class
    Added IControllable
    Added IInteractable

---
+++ Assets/Scripts/Interfaces/IControllable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype
+{
+    public interface IControllable
+    {
+        IEnumerable InteractWith(IInteractable target);
+        void Move(Vector3 direction);
+        void MoveTo(Vector3 position);
+    }
+}
