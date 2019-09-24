681076ae47ae5cac482260fefc6b762bda44e978
Ezassin <Metzger_Gaming@web.de>
2019-09-24 14:18:45 +0200

    First Boid Scripts
    - no functionality
    - just ideas

---
+++ Assets/Scripts/Boid/Boidcontroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype
+{
+    public class Boidcontroller : MonoBehaviour
+    {
+        #region Variables / Properties
+        private Dictionary<int, Boidling> _boidlingsDic;
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+            _boidlingsDic = new Dictionary<int, Boidling>();
+        }
+
+        public void ConnectToController(Boidling connectingBoid)
+        {
+            _boidlingsDic.Add(connectingBoid.GetInstanceID(), connectingBoid);
+        }
+
+        public void DisconnectFromController(Boidling disconnectingBoid)
+        {
+            _boidlingsDic.Remove(disconnectingBoid.GetInstanceID());
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Boid/Boidling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype
+{
+    public class Boidling : MonoBehaviour
+    {
+        #region Variables / Properties
+
+        private Vector3 _connectedCenter;
+        public Vector3 ConnectedCenter { get { return _connectedCenter; } set { _connectedCenter = value; } }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
