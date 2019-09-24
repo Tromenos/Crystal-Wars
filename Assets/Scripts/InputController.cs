e702826f9abc68f6aed671c6ffa78fbb6f242812
Simon Meyer <meyersimon8@gmail.com>
2019-09-24 14:46:12 +0200

    Selection + Visualization
    Selection does not work perfectly yet

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Prototype
@@ -11,10 +12,10 @@ namespace Prototype
         [SerializeField]
         private float _selectionRadius = 15f;
         [SerializeField]
-        private int _selectionLayer;
+        private int _selectionLayer, _planeLayer;
         [SerializeField]
         private float _maxUnitHeight = 15f;
-        Coroutine _boxSelection;
+        Coroutine _boxSelection = null;
         [SerializeField]
         private List<Unit> _selected = new List<Unit>();
 
@@ -24,16 +25,22 @@ namespace Prototype
         private void Start()
         {
             _selectionLayer = 1 << _selectionLayer;
+            _planeLayer = 1 << _planeLayer;
+
+            Selection.Cam = _cam;
+            Selection.SelectionLayer = _selectionLayer;
+            Selection.SelectionRadius = _selectionRadius;
         }
 
         private void FixedUpdate()
         {
             var ray = _cam.ScreenPointToRay(Input.mousePosition);
-            bool hasHit = Physics.Raycast(ray, out RaycastHit hit);
+            bool hasHit = Physics.Raycast(ray, out RaycastHit hit, _planeLayer);
+
 
             if(Input.GetMouseButtonDown(0) && hasHit)
                 if(_selected != null)
-                    CastSphereSelection(hit);
+                    Selection.CastSphereSelection(hit);
 
             if(Input.GetMouseButton(0) && hasHit && _selected != null && _boxSelection == null)
                 _boxSelection = StartCoroutine(BoxSelectionRoutine(new Vector3(hit.point.x, -_maxUnitHeight, hit.point.z)));
@@ -43,37 +50,6 @@ namespace Prototype
                     unit.MoveTo(hit.point);
 
         }
-
-        private void CastSphereSelection(RaycastHit hit)
-        {
-            var selected = Physics.OverlapSphere(hit.point, _selectionRadius, _selectionLayer);
-
-            AddSelection(selected);
-        }
-
-        private void AddSelection(Collider[] selection)
-        {
-            if(selection != null && selection.Length >= 1)
-            {
-                foreach(var collider in selection)
-                {
-                    var unit = collider.GetComponent<Unit>();
-
-                    if(unit)
-                        _selected.Add(unit);
-                }
-            }
-            else if(selection.Length <= 0)
-                _selected.Clear();
-        }
-
-        private void CastBoxSelection(Vector3 pos1, Vector3 pos2)
-        {
-            var parameter = CalculateBoxParameter(pos1, pos2);
-            var selected = Physics.OverlapBox(parameter.Item1, parameter.Item2, Quaternion.identity, _selectionLayer);
-
-            AddSelection(selected);
-        }
         private IEnumerator BoxSelectionRoutine(Vector3 pos1)
         {
             RaycastHit hit;
@@ -81,20 +57,21 @@ namespace Prototype
             {
                 if(Input.GetMouseButtonDown(1))
                 {
+                    center = Vector3.zero;
+                    size = Vector3.zero;
                     yield break;
                 }
-                if(Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out hit))
+                if(Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out hit, _planeLayer))
                 {
-                    var param = CalculateBoxParameter(pos1, hit.point + Vector3.up * _maxUnitHeight);
-                    center = param.Item1;
-                    size = param.Item2;
-                    //DrawCube(param.Item1, param.Item2);
+                    var param = Selection.CalculateBoxParameter(pos1, hit.point + Vector3.up * _maxUnitHeight);
+                    center = param.center;
+                    size = param.extents;
                 }
                 yield return new WaitForEndOfFrame();
             }
 
             Vector3 pos2;
-            if(Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out hit))
+            if(Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out hit, _planeLayer))
             {
                 if(hit.point != pos1)
                 {
@@ -103,65 +80,18 @@ namespace Prototype
 
                     pos1 = new Vector3(pos1.x, -_maxUnitHeight, pos1.z);
 
-                    CastBoxSelection(pos1, pos2);
+                    Selection.CastBoxSelection(pos1, pos2);
                 }
             }
             _boxSelection = null;
         }
 
-        private (Vector3, Vector3) CalculateBoxParameter(Vector3 pos1, Vector3 pos2)
-        {
-            if(pos2.z < pos1.z)
-            {
-                var z = pos2.z;
-                pos2.z = pos1.z;
-                pos1.z = z;
-            }
-            if(pos2.x < pos1.x)
-            {
-                var x = pos2.x;
-                pos2.x = pos1.x;
-                pos1.x = x;
-            }
-
-            var vec = pos2 - pos1;
-
-            Vector3 halfExtents = vec * 0.5f;
-
-            return (pos2 - halfExtents
-                    , halfExtents);
-        }
-
         private void OnDrawGizmos()
         {
-            Gizmos.DrawCube(center, size);
-        }
-
-        private void DrawCube(Vector3 center, Vector3 size)
-        {
-            var bLR = center - size;
-            var bRR = new Vector3(bLR.x + size.x, bLR.y, bLR.z);
-            var bLL = new Vector3(bLR.x, bLR.y, bLR.z + size.z);
-            var bRL = new Vector3(bRR.x, bRR.y, bRR.z + size.z);
-            var tRL = center + size;
-            var tLR = new Vector3(bLR.x, tRL.y, bLR.z);
-            var tLL = new Vector3(bLL.x, tRL.y, bLL.z);
-            var tRR = new Vector3(bRR.x, tRL.y, bRR.z);
-
-            Color c = Color.green;
-
-            Debug.DrawRay(bLR, bRR, c, 100f);
-            Debug.DrawRay(tLR, tRR, c, 100f);
-            Debug.DrawRay(bLL, bRL, c, 100f);
-            Debug.DrawRay(tLL, tRL, c, 100f);
-            Debug.DrawRay(bLR, bLL, c, 100f);
-            Debug.DrawRay(tLR, tLL, c, 100f);
-            Debug.DrawRay(bRR, bRL, c, 100f);
-            Debug.DrawRay(tRR, tRL, c, 100f);
-            Debug.DrawRay(tLR, bLR, c, 100f);
-            Debug.DrawRay(tLL, bLL, c, 100f);
-            Debug.DrawRay(tRR, bRR, c, 100f);
-            Debug.DrawRay(tRL, bRL, c, 100f);
+            var c = Gizmos.color;
+            Gizmos.color = Color.black;
+            Gizmos.DrawWireCube(center, size * 2);
+            Gizmos.color = c;
         }
     }
 }
---
+++ Assets/Scripts/Selection.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Prototype
+{
+    public static class Selection
+    {
+        private static List<IControllable> _selected = new List<IControllable>();
+
+        public static Camera Cam;
+        public static float SelectionRadius = 15f;
+        public static int SelectionLayer;
+
+        public static IControllable[] Selected => _selected?.ToArray();
+
+        public static void CastSphereSelection(RaycastHit hit)
+        {
+            var selected = Physics.OverlapSphere(hit.point, SelectionRadius, SelectionLayer);
+
+            AddSelection(selected);
+        }
+
+        private static void AddSelection(Collider[] selection)
+        {
+            if(selection != null && selection.Length >= 1)
+            {
+                foreach(var collider in selection)
+                {
+                    var unit = collider.GetComponent<Unit>();
+
+                    if(unit)
+                        _selected.Add(unit);
+                }
+            }
+            else if(selection.Length <= 0)
+                _selected.Clear();
+        }
+
+        public static void CastBoxSelection(Vector3 pos1, Vector3 pos2)
+        {
+            var (center, extents) = CalculateBoxParameter(pos1, pos2);
+            var selected = Physics.OverlapBox(center, extents, Cam.transform.rotation, SelectionLayer);
+
+            AddSelection(selected);
+        }
+
+
+        public static (Vector3 center, Vector3 extents) CalculateBoxParameter(Vector3 pos1, Vector3 pos2)
+        {
+            var vec = (pos2 - pos1) * 0.5f;
+
+            return (pos1 + vec, math.abs(vec));
+        }
+    }
+}
